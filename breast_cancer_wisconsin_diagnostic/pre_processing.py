4c4fc793bff77f60505a96f1083103d3605946c7
mikaeletha <mikaelethainara@gmail.com>
2024-11-13 13:10:01 +0000

    pre processed

--- breast_cancer_wisconsin_diagnostic/pre_processing.py
+++ breast_cancer_wisconsin_diagnostic/pre_processing.py
@@ -1,42 +1,54 @@
-import pandas
-import matplotlib.pyplot as plt
-# import seaborn
+import pandas as pd
+import matplotlib.pyplot as pl
+from sklearn.model_selection import train_test_split
+
 
 def remove_duplicate_rows(df):
-    # Count the number of rows before removing duplicates
     initial_count = len(df)
-
-    # Remove duplicates
     df_cleaned = df.drop_duplicates()
-
-    # Count the number of rows after removing duplicates
     final_count = len(df_cleaned)
-
-    # Calculate the number of duplicates removed
     duplicates_removed = initial_count - final_count
 
-    # Print the number of duplicates removed
     if duplicates_removed > 0:
-        print({duplicates_removed})
+        print(f"{duplicates_removed} duplicatas removidas.")
     else:
-        print("No duplicate samples were found.")
-
-    # print(f"Dataset contains {final_count} Samples")
+        print("Nenhuma amostra duplicada encontrada.")
 
     return df_cleaned
 
-column_names = ['ID', 'Diagnosis', 'radius1', 'texture1', 'perimeter1', 'area1', 'smoothness1', 'compactness1', 'concavity1', 'concave_points1', 'symmetry1', 'fractal_dimension1', 'radius2', 'texture2', 'perimeter2', 'area2', 'smoothness2', 'compactness2', 'concavity2', 'concave_points2', 'symmetry2', 'fractal_dimension2', 'radius3', 'texture3', 'perimeter3', 'area3', 'smoothness3', 'compactness3', 'concavity3', 'concave_points3', 'symmetry3', 'fractal_dimension3']
 
-dataset = pandas.read_csv("./data/wdbc.data", header=None, names = column_names)
+def remove_missing_values(dataset):
+    if dataset.isnull().values.any():
+        print("Valores ausentes encontrados. Removendo valores ausentes...")
+        dataset = dataset.dropna()
+    else:
+        print("Nenhum valor ausente encontrado.")
+    return dataset
+
+
+column_names = ['ID', 'Diagnosis', 'radius1', 'texture1', 'perimeter1', 'area1', 'smoothness1', 'compactness1', 'concavity1', 'concave_points1', 'symmetry1', 'fractal_dimension1', 'radius2', 'texture2', 'perimeter2', 'area2', 'smoothness2',
+                'compactness2', 'concavity2', 'concave_points2', 'symmetry2', 'fractal_dimension2', 'radius3', 'texture3', 'perimeter3', 'area3', 'smoothness3', 'compactness3', 'concavity3', 'concave_points3', 'symmetry3', 'fractal_dimension3']
+
+dataset = pd.read_csv(
+    "breast_cancer_wisconsin_diagnostic/data/wdbc.data", header=None, names=column_names)
 
 dataset = remove_duplicate_rows(dataset)
+dataset = remove_missing_values(dataset)
 
 print(dataset.describe(include="all"))
 
-num_variables = len(dataset.columns)
-num_output_variables = 1
-number_input_variables = num_variables - num_output_variables
+x = dataset.drop(columns=['Diagnosis'])
+t = dataset['Diagnosis']
 
-input_columns = dataset.columns[0:number_input_variables]
+x_train, x_test, t_train, t_test = train_test_split(
+    x, t, train_size=0.5, stratify=t)
 
-dataset.to_csv("pre_processed/wdbc.csv", index=False)
+train = pd.concat([x_train, t_train], axis='columns', join='inner')
+test = pd.concat([x_test, t_test], axis='columns', join='inner')
+
+train.to_csv(
+    "breast_cancer_wisconsin_diagnostic/pre_processed/wdbc_train.csv", index=False)
+test.to_csv(
+    "breast_cancer_wisconsin_diagnostic/pre_processed/wdbc_test.csv", index=False)
+dataset.to_csv(
+    "breast_cancer_wisconsin_diagnostic/pre_processed/wdbc.csv", index=False)
